8a354a5043e8988adf3821dedf324d034a216318
Evgeny Yachny <ev.yachny@gmail.com>
2022-12-09 23:01:36 +0300

    Initial Commit

---
+++ SocialMedia/SocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaApp.swift
+//  SocialMedia
+//
+//  Created by Evgeny on 9.12.22.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
